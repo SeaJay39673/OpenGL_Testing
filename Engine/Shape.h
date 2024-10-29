b2c871cc02c4ca33d9d82297b77ad8b9cf6a1071
Christopher Edmunds <chris.easy15@gmail.com>
2024-10-29 09:41:28 -0600

    update: add comments to Shape class

--- Engine/Shape.h
+++ Engine/Shape.h
@@ -50,6 +50,7 @@ public:
 };
 
 /**
+    @details Constructor for shape class. Creates the VBO with passed in data.
     @param type (GLenum) specify type of drawing method (STATIC or DYNAMIC)
     @param vertices (float*) specify vertex data for VBO
     @param vSize (int) specify size of vertex data for VBO
@@ -60,6 +61,7 @@ Shape::Shape(GLenum type, float *vertices, int vSize) : transform(glm::mat4(1.0f
 }
 
 /**
+    @details Constructor for shape class. Creates the VBO and EBO with passed in data.
     @param type (GLenum) specify type of drawing method (STATIC or DYNAMIC)
     @param vertices (float*) specify vertex data for VBO
     @param vSize (int) specify size of vertex data for VBO
@@ -71,6 +73,11 @@ Shape::Shape(GLenum type, float *vertices, int vSize, unsigned int *indices, int
     UpdateData(vertices, vSize, indices, iSize);
 }
 
+/**
+    @details Updates the VBO with passed in data, determines how the draw method should function based on data.
+    @param vertices (float*) specify vertex data for VBO
+    @param vSize (int) specify size of vertex data for VBO
+ */
 void Shape::UpdateData(float *vertices, int vSize)
 {
     Bind();
@@ -78,6 +85,13 @@ void Shape::UpdateData(float *vertices, int vSize)
     drawMethod = Triangles;
 }
 
+/**
+    @details Updates the VBO and EBO with passed in data, determines how the draw method should function based on data.
+    @param vertices (float*) specify vertex data for VBO
+    @param vSize (int) specify size of vertex data for VBO
+    @param indices (unsigned int*) specify index data for EBO
+    @param iSize (int) specify size of index data for EBO
+*/
 void Shape::UpdateData(float *vertices, int vSize, unsigned int *indices, int iSize)
 {
     UpdateData(vertices, vSize);
@@ -85,34 +99,54 @@ void Shape::UpdateData(float *vertices, int vSize, unsigned int *indices, int iS
     drawMethod = Elements;
 }
 
+/**
+    @details Specify how the data should be interpreted by the shader
+    @param layout (GLuint) which layout (location) in the shader will read in the data
+    @param elements (int) how many elements are there to read per vertex
+    @param span (int) how many elements to be skipped before the next vertex can be read
+    @param index (int) the index of data to start reading in on the shader
+*/
 void Shape::SetVertexPointer(GLuint layout, int elements, int span, int index)
 {
     Bind();
     vao.LinkVB(vbo, layout, elements, span, index);
 }
-
+/**
+    @details Binds the shape object (VAO, VBO, EBO, Texture)
+ */
 void Shape::Bind()
 {
     vao.Bind();
     vbo.Bind();
     ebo.Bind();
-    // tex.Bind();
+    tex.Bind();
 }
 
+/**
+    @details Unbinds the shape object (VAO, VBO, EBO, Texture)
+ */
 void Shape::Unbind()
 {
     vao.Unbind();
     vbo.Unbind();
     ebo.Unbind();
-    // tex.Unbind();
+    tex.Unbind();
 }
 
+/**
+    @details Specify how the draw method should function
+    @param first (int) index of the first element to be read
+    @param elements (int) number of elements to draw (vertices or indices)
+ */
 void Shape::SetDrawData(int first, int elements)
 {
     drawFirst = first;
     drawElements = elements;
 }
 
+/**
+    @details Uses the provided shader, binds the object, calls its draw function, and unbinds.
+ */
 void Shape::Draw()
 {
     shader.use();
@@ -131,17 +165,31 @@ void Shape::Draw()
     Unbind();
 }
 
+/**
+    @brief Sets the current texture
+    @details Pass in a texture object, this class receives it by reference.
+    @param txtr (Texture&) The texture object to be passed in
+ */
 void Shape::SetTexture(Texture &txtr)
 {
     Bind();
     tex = txtr;
 }
 
+/**
+    @brief Sets the current shader
+    @details Pass in a shader object, this class receives it by reference
+    @param shdr (Shader&) The shader object to be passed in
+ */
 void Shape::SetShader(Shader &shdr)
 {
     shader = shdr;
 }
 
+/**
+    @details Rotate the shape by a given angle (Radians)
+    @param angle (float) The angle to be rotated by
+ */
 void Shape::Rotate(float angle)
 {
     // transform = glm::translate(transform, glm::vec3(0, 0, 0));
@@ -150,11 +198,19 @@ void Shape::Rotate(float angle)
     rotation += angle;
 }
 
+/**
+    @details Scale the shape by a given scalar
+    @param scalar (float) the scalar to be scaled by
+ */
 void Shape::Scale(float scalar)
 {
     transform = glm::scale(transform, glm::vec3(scalar, scalar, 1));
 }
 
+/**
+    @details Translate the shape by a given translation vector
+    @param trans (glm::vec3) The translation vector to be applied to the shape
+ */
 void Shape::Translate(glm::vec3 trans)
 {
     transform = glm::translate(transform, trans);
